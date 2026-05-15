b1621b820787719733b296a48d4bdd366e37e4db
mehul99agr <mehul.agrawal99@gmail.com>
2026-05-15 22:12:15 +0530

    fix: remove www redirect from Next.js to fix redirect loop

--- next.config.js
+++ next.config.js
@@ -9,16 +9,6 @@ const securityHeaders = [
 
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.agrawalkhandelwal.com' }],
-        destination: 'https://agrawalkhandelwal.com/:path*',
-        permanent: true,
-      },
-    ]
-  },
   async headers() {
     return [
       {
